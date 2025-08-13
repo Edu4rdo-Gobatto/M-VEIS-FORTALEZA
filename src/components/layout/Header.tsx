64005207b4430c09dbaeaeb33151de28bb349e0a
EDUARDO GOBATTO <eduardogobt@gmail.com>
2025-08-13 17:32:17 -0400

    Itens do header selecionados

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,18 +2,26 @@ export default function Header() {
 
   return (
 
-    <header className="bg-blue-600 flex justify-between h-full">
+    <header className="bg-blue-600 flex justify-between h-full w-screen p-3">
 
-      <div className="">
+      <div className="max-w-8 max-h-8">
         <img src="./engrenagem.png" alt="config"
-          className="h-8 w-8 m-2"
+          className="w-full h-full"
         />
       </div>
-      <div className="m-2">
-        nav - bar
+
+      <div className="flex m-0 p-0 text-white font-semibold">
+        <ul className="inline-flex space-x-3">
+          <li>Pesquise</li>
+          <li>MÓVEIS FORTALEZA</li>
+          <li>FALE CONOSCO</li>
+        </ul>
       </div>
-      <div className="m-2">
-        Login
+
+      <div className="max-w-8 max-h-8">
+        <img src="carrinho.png" alt="" 
+          className="w-full h-full"
+        />
       </div>
       
     </header>
